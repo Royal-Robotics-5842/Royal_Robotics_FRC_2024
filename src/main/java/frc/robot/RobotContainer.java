6d099aafef0e18f2673f7aa5f49894f9b05b0010
Krish Gupta <cozmickrish@gmail.com>
2024-01-28 18:10:54 -0500

    ?

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -26,6 +26,7 @@ import edu.wpi.first.math.trajectory.Trajectory;
 import edu.wpi.first.math.trajectory.TrajectoryConfig;
 import edu.wpi.first.math.trajectory.TrajectoryGenerator;
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
@@ -65,7 +66,7 @@ public class RobotContainer {
 
 
         //AUTO STUFFF
-         
+        private final SendableChooser<Command> autoChooser;
 
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
@@ -80,6 +81,13 @@ public class RobotContainer {
       () -> true));
 
       NamedCommands.registerCommand("Hi", setTo0);
+        // Build an auto chooser. This will use Commands.none() as the default option.
+        autoChooser = AutoBuilder.buildAutoChooser();
+
+        // Another option that allows you to specify the default auto by its name
+        // autoChooser = AutoBuilder.buildAutoChooser("My Default Auto");
+
+        SmartDashboard.putData("Auto Chooser", autoChooser);    
     
   }
 
@@ -126,8 +134,10 @@ public class RobotContainer {
 
     PathPlannerPath Test = PathPlannerPath.fromPathFile("Testing");
 
-    return Commands.runOnce(()->swerveSubsystem.resetOdemetry(LeftNote.getPreviewStartingHolonomicPose()))
-        .andThen(AutoBuilder.followPath(Test));
+    return autoChooser.getSelected();
+    
+    //return Commands.runOnce(()->swerveSubsystem.resetOdemetry(LeftNote.getPreviewStartingHolonomicPose()))
+        //.andThen(AutoBuilder.followPath(Test));
 /*
     // Create a path following command using AutoBuilder. This will also trigger event markers.
         return Commands.runOnce(()->swerveSubsystem.resetOdemetry(LeftNote.getPreviewStartingHolonomicPose()))
